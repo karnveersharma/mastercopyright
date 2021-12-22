1e47b2e40bd19fd8314e4e72d5c5bf8ec24e36fe
Karnveer <karnveersharma1994@gmail.com>
2021-12-23 02:42:17 +0530

    updated styles

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import GroupWorkIcon from '@mui/icons-material/GroupWork';
 import HomeIcon from '@mui/icons-material/Home';
 import PhoneIcon from '@mui/icons-material/Phone';
 import NewspaperIcon from '@mui/icons-material/Newspaper';
+import { AppBar } from '@mui/material';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -52,26 +53,33 @@ function App() {
   return (
     <div className="App">
       <Box sx={{ width: '100%' }}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value} onChange={handleChange} aria-label="menu" textColor="secondary" indicatorColor="secondary">
-            <Tab label="Home" icon={<HomeIcon />} iconPosition="start" {...a11yProps(0)} />
-            <Tab label="About Us" icon={<GroupWorkIcon />} iconPosition="start" {...a11yProps(1)} />
-            <Tab label="Contact" icon={<PhoneIcon />} iconPosition="start" {...a11yProps(2)} />
-            <Tab label="News" icon={<NewspaperIcon />} iconPosition="start" {...a11yProps(3)} />
-          </Tabs>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider', bgColor: 'background.paper' }}>
+          <AppBar position="static">
+            <Tabs value={value} onChange={handleChange} aria-label="menu" textColor="inherit" indicatorColor="secondary">
+              <Tab label="Home" icon={<HomeIcon />} iconPosition="start" {...a11yProps(0)} />
+              <Tab label="About Us" icon={<GroupWorkIcon />} iconPosition="start" {...a11yProps(1)} />
+              <Tab label="Contact" icon={<PhoneIcon />} iconPosition="start" {...a11yProps(2)} />
+              <Tab label="News" icon={<NewspaperIcon />} iconPosition="start" {...a11yProps(3)} />
+            </Tabs>
+          </AppBar>
         </Box>
         <TabPanel value={value} index={0}>
-
+          <h1>
+            Welcome To Master Copyright
+          </h1>
         </TabPanel>
         <TabPanel value={value} index={1}>
           <p>
-            Master Copyright-The Leading Brand In Indian Entertainment Now Bring You All The Updates
-          </p>  
+            <h3>Master Copyright - </h3>
+            The Leading Brand In Indian Entertainment Now Bring You All The Updates
+          </p>
           <p>
             On Yours Our Latest And Blockmaster Hits Only On Our OFficial Youtube Page Master Copyright Lucknow.
-          </p>  
+          </p>
           <p>
-            Stay Tuned!
+            <b style={{ color: '#ff0000' }}>
+              Stay Tuned!
+            </b>
           </p>
           <p>
             Master Copyright Firms work to providing "NOC" for music's commercial purpose under the Indian
@@ -81,9 +89,9 @@ function App() {
           </p>
           <p>
             commercial use without "NOC".
-          </p> 
+          </p>
           <p>
-            CD,VCD,DVD,MP3,Memory chip,Downloading D.J.System.Event Show Club, Hotels are also in its working
+            CD, VCD, DVD, MP3, Memory chip, Downloading, D.J.System, Event Show, Clubs, Hotels are also in its working
           </p>
           <p>
             criteria. Master Copyright is working for films producers.The Firm works Delhi, Madhya Pradesh,
@@ -96,28 +104,37 @@ function App() {
           </p>
 
           <p>
-            Upcoming Artist Here is Your Chance To Make It Big With us, Send it Entries On Whatsapp Number 9425734148
-          </p>          
+            Upcoming Artist Here is Your Chance To Make It Big With us,
+          </p>
+          <p>
+            <b>
+              Send it Entries On Whatsapp Number 9425734148
+            </b>
+          </p>
         </TabPanel>
         <TabPanel value={value} index={2}>
           <p>
-            ADDRESS
+            <b>
+              ADDRESS
+                </b>
           </p>
           <p>
-            Head Office:
+            <b>
+              Head Office:
+                </b>
           </p>
           <p>
             7/738, Jankipuram Vistar,
-          </p>
+              </p>
           <p>
             Lucknow, UTTAR PRADESH, India
-          </p>
+              </p>
           <p>
-            Phone1: +91 94257-34148
-          </p>
+            <b>Phone1: </b> +91 94257-34148
+              </p>
           <p>
-            Phone2: +91 74152-63331
-          </p>
+            <b>Phone2: </b> +91 74152-63331
+              </p>
         </TabPanel>
         <TabPanel value={value} index={3}>
 
